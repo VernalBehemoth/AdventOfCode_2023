547cba2eb6fec0992bdcde507786eba395fcd7f1
Graham Voisey <graham.voisey@wiccon.se>
2023-12-01 09:50:30 +0100

    Add project files.

---
+++ AdventOfCode/Day1.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.RegularExpressions;
+
+public class Day1 : IDayTask
+{
+    public void RunTask()
+    {
+        Console.WriteLine("Day 1 Executed!");
+
+        string path = "./Data/day1.txt"; // Change this to the path of your text file
+
+        try
+        {
+            string[] lines = File.ReadAllLines(path);
+            var sum = 0;
+            foreach (string line in lines)
+            {
+                // Extracting numbers from the line
+                var numbers = Regex.Matches(line, @"\d+").Cast<Match>().Select(m => m.Value).ToArray();
+
+                if (numbers.Length > 0)
+                {
+                    string firstNumber = numbers.First().Substring(0, 1);
+                    string lastNumber = numbers.Last().Substring(numbers.Last().Length - 1);
+
+                    int.TryParse(firstNumber + lastNumber, out var concatenatedInt);
+
+                    sum += concatenatedInt;
+
+                    Console.WriteLine(concatenatedInt);
+                }
+            }
+
+            Console.WriteLine("Total: " + sum);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+
+    }
+}
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,35 @@
+﻿bool keepRunning = true;
+while (keepRunning)
+{
+    Console.WriteLine("Select the Day's Task to Run:");
+    Console.WriteLine("1. Day 1");
+    Console.WriteLine("2. Day 2");
+    // Add more days as you implement them
+    Console.WriteLine("0. Exit");
+
+    var selection = Console.ReadLine();
+
+    switch (selection)
+    {
+        case "1":
+            new Day1().RunTask();
+            break;
+        case "2":
+            Console.WriteLine("Soon");
+            break;
+        // Add more cases as needed
+        case "0":
+            keepRunning = false;
+            break;
+        default:
+            Console.WriteLine("Invalid selection, please try again.");
+            break;
+    }
+
+    if (keepRunning)
+    {
+        Console.WriteLine("\nPress any key to return to the menu...");
+        Console.ReadKey();
+        Console.Clear();
+    }
+}
